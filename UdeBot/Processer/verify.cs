a5c1c6a9f7aa340e43ee36eb065866145d18e6fb
hafniz <42380178+hafniz@users.noreply.github.com>
2018-11-29 12:45:56 +0800

    Fixed issue #IP6A5 by forcing GenCode() generate verificationCode containing at least one non-numeral chars

--- UdeBot/Processer/verify.cs
+++ UdeBot/Processer/verify.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Timers;
@@ -78,16 +79,20 @@ namespace UdeBot.Processer
         private void GenCode()
         {
             Random random = new Random();
-            int Determiner(int n) => n == 0 ? random.Next(0x30, 0x3a) : (n == 1 ? random.Next(0x41, 0x5b) : random.Next(0x61, 0x7b));
-            //Console.Write();
-            //Random random = new Random();
+            int GenerateChar(int n) => n == 0 ? random.Next(0x30, 0x3a) : (n == 1 ? random.Next(0x41, 0x5b) : random.Next(0x61, 0x7b)); // parameter n corresponds to type of type of chars: 0 being numerals, 1 being upper-case alphabets, and 2 being lower-case alphabets
             var code = "";
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < 8; i++)
             {
-                //int determiner(int n) => 
-                code += (char)Determiner(random.Next(3));
+                code += (char)GenerateChar(random.Next(3)); // randomly decide whether to generate a numeral, an upper-case alphabet or a lower-case alphabet
             }
-
+            if (code.Any(c => (c >= 0x41 && c < 0x5b) || (c >= 0x61 && c < 0x7b))) // if there are any alphabets (i.e. non-numerals) in code
+            {
+                code += (char)GenerateChar(random.Next(3));
+            }
+            else
+            {
+                code += (char)GenerateChar(random.Next(1, 3)); // force generate a non-numeral char at the last place, in case all 8 leading chars are numerals (in a 0.015% chance)
+            }
             verificationCode = code;
         }
 
@@ -112,7 +117,7 @@ namespace UdeBot.Processer
                     DealWithInvalidVerification();
                 }
             }
-            else // input is probably generated random code for account binding
+            else // input is probably generated random code for account binding. GenCode() method is modified to ensure that the verification code generated includes at least one non-numeral char. 
             {
                 isSuccessful = verificationCode == inputCode;
                 if (isSuccessful)
